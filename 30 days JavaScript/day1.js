6f534cfa0122707898dfab32a8592a96f4db7903
Aroskar17 <20co13@aitdgoa.edu.in>
2023-05-05 23:36:10 +0530

    Day One Solution

---
+++ 30 days JavaScript/day1.js
@@ -0,0 +1,16 @@
+/**
+ LeetCode 30 days JavaScript challenge 
+ Day 1 Solution   
+ 
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
